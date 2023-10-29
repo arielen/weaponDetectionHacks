e91592b0218f9d0c22e75a8ec4ec067352ee18dd
poryadok <almazbek1608@gmail.com>
2023-10-29 06:03:34 +0100

    allow all hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = []
 # Get csrf token from .env file
 if os.path.exists(BASE_DIR / '.env'):
     with open(BASE_DIR / '.env') as f:
